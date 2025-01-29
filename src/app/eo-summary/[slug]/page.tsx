560b3cebe01351c1a253c79566c321af95b624a8
CuriouslyCory <cory@hau.me>
2025-01-29 14:25:48 -0700

    Wrap details

--- src/app/eo-summary/[slug]/page.tsx
+++ src/app/eo-summary/[slug]/page.tsx
@@ -74,37 +74,40 @@ export default async function Page({ params }: Props) {
 
   return (
     <div className="">
-      <div className="mb-6 flex flex-col gap-y-2">
-        <h1 className="mb-6 text-2xl font-bold">
-          {toTitleCase(document?.title)}
-        </h1>
-        <div className="flex items-center gap-x-2">
-          <span className="text-gray-600 dark:text-gray-400">
-            Constitutional Risk:{" "}
-          </span>
-          <RiskScore score={document?.riskScore} />
-        </div>
-        <div>
-          <span className="text-gray-600 dark:text-gray-400">Signed by:</span>{" "}
-          <span className="font-bold">{document?.signer}</span>
-        </div>
-        <div>
-          <span className="text-gray-600 dark:text-gray-400">Signed:</span>{" "}
-          <span className="font-bold">
-            {document?.dateSigned.toLocaleDateString()}
-          </span>
-        </div>
-        {document?.updatedAt.toISOString() !==
-          document?.createdAt.toISOString() && (
-          <div>
+      <h1 className="mb-6 text-2xl font-bold">
+        {toTitleCase(document?.title)}
+      </h1>
+
+      <div className="mb-12 w-fit rounded-md border bg-card p-6 text-card-foreground dark:border-gray-700">
+        <div className="flex flex-col gap-x-2 gap-y-2">
+          <div className="flex items-center gap-x-2">
             <span className="text-gray-600 dark:text-gray-400">
-              Last Updated:
-            </span>{" "}
+              Constitutional Risk:{" "}
+            </span>
+            <RiskScore score={document?.riskScore} />
+          </div>
+          <div>
+            <span className="text-gray-600 dark:text-gray-400">Signed by:</span>{" "}
+            <span className="font-bold">{document?.signer}</span>
+          </div>
+          <div>
+            <span className="text-gray-600 dark:text-gray-400">Signed:</span>{" "}
             <span className="font-bold">
-              {document?.updatedAt.toLocaleDateString()}
+              {document?.dateSigned.toLocaleDateString()}
             </span>
           </div>
-        )}
+          {document?.updatedAt.toISOString() !==
+            document?.createdAt.toISOString() && (
+            <div>
+              <span className="text-gray-600 dark:text-gray-400">
+                Last Updated:
+              </span>{" "}
+              <span className="font-bold">
+                {document?.updatedAt.toLocaleDateString()}
+              </span>
+            </div>
+          )}
+        </div>
       </div>
       <div className="flex flex-col gap-y-6">
         <section className="flex flex-col">
@@ -122,6 +125,9 @@ export default async function Page({ params }: Props) {
               <div className="mb-4 flex items-center gap-x-2">
                 <ChevronsUpDown className="h-4 w-4" />
                 <h2 className="text-lg font-bold">{artifact.title}</h2>
+                <span className="text-gray-600 dark:text-gray-400">
+                  (Click to expand)
+                </span>
               </div>
             </CollapsibleTrigger>
             <CollapsibleContent className="CollapsibleContent">
