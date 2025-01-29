abd4d8bfcd2a410957355d0a48d6bfe38f1b4822
Cory Sougstad <cory@hau.me>
2025-01-28 20:52:58 -0700

    Fix order of report items

--- src/server/api/routers/document.ts
+++ src/server/api/routers/document.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
-
+import { type Document, type DocumentArtifact } from "@prisma/client";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const documentRouter = createTRPCRouter({
@@ -21,6 +21,29 @@ export const documentRouter = createTRPCRouter({
           documentArtifact: true,
         },
       });
-      return document;
+      if (!document) {
+        throw new Error("Document not found");
+      }
+      return {
+        ...document,
+        documentArtifact: artifactOrder
+          .map((title) => getArtifactByTitle(document, title))
+          .filter((artifact) => artifact !== undefined),
+      };
     }),
 });
+
+const artifactOrder = [
+  "Key Points",
+  "Constitutional Considerations",
+  "Take Action",
+  "Final Summary",
+  "Original Document",
+];
+
+function getArtifactByTitle(
+  document: Document & { documentArtifact: DocumentArtifact[] },
+  title: string,
+) {
+  return document.documentArtifact.find((artifact) => artifact.title === title);
+}
