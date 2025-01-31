ae182e47733fafa3410cb9f38d2ecbc50c91ca57
Cory Sougstad <cory@hau.me>
2025-01-31 11:59:05 -0700

    Remove old mdx config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
 // @ts-check
-
-import createMDX from "@next/mdx";
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
@@ -9,14 +7,8 @@ import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
 const config = {
-  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+  pageExtensions: ["js", "jsx", "ts", "tsx"],
 };
 
-const withMDX = createMDX({
-  // By default only the `.mdx` extension is supported.
-  extension: /\.mdx?$/,
-  // Add markdown plugins here, as desired
-});
-
 // Merge MDX config with Next.js config
-export default withMDX(config);
+export default config;
