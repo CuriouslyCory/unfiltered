3ad84dbc92a261c5748f2b301de4335d6c8205e9
Cory Sougstad <cory@hau.me>
2025-03-25 10:47:54 -0700

    Fix import issue.

--- src/app/_components/document-type-badge.tsx
+++ src/app/_components/document-type-badge.tsx
@@ -1,5 +1,5 @@
 import { type DocumentType } from "@prisma/client";
-import { HTMLAttributes } from "react";
+import { type HTMLAttributes } from "react";
 import { Badge } from "~/components/ui/badge";
 import { cn } from "~/lib/utils";
 
